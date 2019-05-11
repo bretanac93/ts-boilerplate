a87d38a6a1dc3d70f91a081f48b9de48839743bf
Cesar Bretana Gonzalez <bretanac@gmail.com>
2019-05-11 14:28:04 +0200

    Using error handling middleware in server entrypoint

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,23 @@ import express from "express";
 import { applyMiddleware, applyRoutes } from "./utils";
 import middleware from "./middleware";
 import routes from "./services";
+import errorHandlers from "./middleware/errorHandlers";
+
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+})
 
 const router = express();
 
 applyMiddleware(middleware, router);
 applyRoutes(routes, router);
+applyMiddleware(errorHandlers, router);
 
 const port = process.env.APP_PORT || 8000;
 
